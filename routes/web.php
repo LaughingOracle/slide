464d3764665e574575d0e88776d7ded5b2fd02a5
LaughingOracle <jms.jordab@gmail.com>
2025-07-23 16:49:28 +0700

    init commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SlideController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/slide/{tv}', [SlideController::class, 'index'])->name('slide.tv');
+
+Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+Route::post('/slide', [AdminController::class, 'store'])->name('slide.store');
+
+Route::get('/search', [SlideController::class, 'search']);
+

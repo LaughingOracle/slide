e9751dc2e3f03e7f2543669646818c32530377e9
LaughingOracle <jms.jordab@gmail.com>
2025-07-30 16:50:45 +0700

    better adjustment with different css for both vertical and horizontal

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -23,11 +23,59 @@
             height: 10%;
             width: 50%;
             border-radius: 15px;
-            font-size: 1.3em;
+            font-size: 10vw;
             cursor: pointer;
             z-index: 100;
             box-shadow: 0 4px 8px rgba(0,0,0,0.3);
         }
+
+        @media (orientation: portrait) {
+            .back-button {
+                opacity: 0;
+                visibility: hidden;
+                transition: opacity 0.5s ease, visibility 0s linear 0.5s; /* delay visibility hide */
+                position: fixed;
+                bottom: 20px;
+                left: 20px;
+                background-color: #3490dc;
+                color: white;
+                border: none;
+                padding: 14px 18px;
+                height: 10%;
+                width: 50%;
+                border-radius: 15px;
+                font-size: 6vw;
+                cursor: pointer;
+                z-index: 100;
+                box-shadow: 0 4px 8px rgba(0,0,0,0.3);
+            }
+        }
+
+        /* Landscape (horizontal) */
+        @media (orientation: landscape) {
+            .back-button {
+                opacity: 0;
+                visibility: hidden;
+                transition: opacity 0.5s ease, visibility 0s linear 0.5s; /* delay visibility hide */
+                position: fixed;
+                bottom: 20px;
+                left: 20px;
+                background-color: #3490dc;
+                color: white;
+                border: none;
+                padding: 14px 18px;
+                height: 15%;
+                width: 30%;
+                border-radius: 15px;
+                font-size: 2vw;
+                cursor: pointer;
+                z-index: 100;
+                box-shadow: 0 4px 8px rgba(0,0,0,0.3);
+            }
+        }
+
+
+
 
         .back-button.visible {
             opacity: 1;
